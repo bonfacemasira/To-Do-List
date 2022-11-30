16d4e3a47f31a5a382d2e19ca4ef983f5c4a5001
Bonface Masira <bonfacemasira@gmail.com>
2022-11-30 09:10:36 +0300

    style live clock display

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from "react";
 import TaskList from './components/TaskList';
 import Background from './components/Background';
+import LiveClock from './components/LiveClock';
 
 function App() {
 
   return (
     <div className="App">
       <Background />
+      <LiveClock />
       <TaskList />
     </div>
   );
